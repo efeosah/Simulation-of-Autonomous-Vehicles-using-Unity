91359f6966d539111f8af562589c0a23a607f2ee
OsamagbeMichael <71562491+OsamagbeMichael@users.noreply.github.com>
2022-03-04 13:11:22 -0500

    Addition of the SocketFloat Script and adding the server python script

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+
+public class client : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
