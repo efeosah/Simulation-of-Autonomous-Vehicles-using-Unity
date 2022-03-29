44b72edd1d9b787690b550d7b1b650880769d417
uhunmwa <uhunmwa@uwindsor.ca>
2022-03-29 09:52:40 -0400

    Working third person camera
    This took way too long to figure out and when i got it i felt dumb because it was really and easy fix .
    Thank you

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,20 +3,41 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
    public Transform target;
-   public float smoothSpeed = 0.125f;
-   public Vector3 offset;
-   public float max_x;
-   public float max_y;
-   public float max_z;
-   //public float desiredAngle = target.eulerAngles.y;
+  //public float smoothSpeed = 0.125f;
+   //public Transform cameraPosition ;
+   
 
+   // public Vector3 offset;
+   // Quaternion offsetRotation;
+
+ 
     // Update is called once per frame
+     // public float distance = 10; 
+     // public float height = 20;
+     // public float heightDamping = 0.25f; 
+     // public float rotationDamping=0.15f ;
+
+   // void start(){
+   //   offsetRotation = transform.rotation * Quaternion.Inverse(target.transform.rotation);
+   //   offset = transform.position - target.transform.position;
+   // }
     void LateUpdate()
-    {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedSpeed = Vector3.Lerp(transform.position,desiredPosition,smoothSpeed);
-        transform.position = smoothedSpeed;
-        //Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
-        transform.LookAt(target);
+
+    { 
+        //Vector3 desiredPosition = target.position + offset;
+         //Vector3 smoothedSpeed = Vector3.Lerp(transform.position,desiredPosition,smoothSpeed);
+         //transform.position = smoothedSpeed;
+
+         //Vector3 resultingPosition = cameraPosition.position + cameraPosition.forward * distance;
+         //transform.position = new Vector3 (resultingPosition.x, transform.position.y, resultingPosition.z);
+         //transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, 0.8f);
+         //transform.rotation = Quaternion.Slerp(target.transform.rotation, target.transform.rotation * offsetRotation, 0.8f);
+
+         //transform.LookAt(new Vector3 (resultingPosition.x, transform.position.y, resultingPosition.z));
+         // Always look at the target
+         transform.LookAt(target);
     }
+
+
+
 }
